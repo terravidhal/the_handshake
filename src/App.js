7dd91de77afa68edae5ed1cbcb8a8b70c0629528
terravidhal <infiniterraetincelle@gmail.com>
2024-01-23 02:42:39 +0100

    the handshake

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import './App.css';
+ 
+
+
 
 function App() {
+  
+  const [socket] = useState(() => io(':8000'));
+ 
+  useEffect(() => {
+    console.log('Is this running?');
+    socket.on('Welcome', data => console.log(data));
+ 
+    return () => socket.off("Welcome");
+  }, [socket]);
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Socket Test</h1>
     </div>
   );
 }
-
+ 
 export default App;
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -6,10 +6,23 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  // </React.StrictMode>
 );
+
+/**
+ * Remarque : si votre application crée deux connexions socket 
+ * à chaque fois que vous chargez le côté client, cela est 
+ * probablement lié au fait que create-react-app encapsule
+ *  désormais le composant App dans un composant React.StrictMode 
+ * pour aider à détecter les problèmes de développement. 
+ * Malheureusement, cela entraîne le déclenchement du rappel 
+ * d'état initial paresseux deux fois. Pour éviter cela, 
+ * supprimez simplement le composant d'emballage dans index.js. 
+ * Consultez le problème suivant sur GitHub : https://github.com/facebook/react/issues/15074 
+ */
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
